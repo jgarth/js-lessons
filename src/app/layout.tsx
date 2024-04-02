06d06eccfe6160c3568763c52268909853f15041
Josh Garth <jpgarth@gmail.com>
2024-04-02 17:10:16 +1100

    Adjust header color & transparency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="bg-gray-100 text-gray-900 text-lg">
       <body className={`bg-gray-100 text-gray-900`}>
-        <nav className="fixed top-0 inset-x-0 h-12 bg-gray-100 z-[1000]">
+        <nav className="fixed top-0 inset-x-0 h-24 bg-gradient-to-b from-gray-100 via-gray-100 to-transparent z-[1000]">
           <Image src={logo} alt="Take2 logo" width={200} className="py-2 px-4 h-12"/>
         </nav>
         <main className="mt-12 p-8 relative">
